c3ede8761733be79b5a6993369730a7b02a32172
javierwelch <welch.javier@gmail.com>
2023-01-15 13:44:39 -0300

    Answered Question 4

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 import seaborn as sns
 import numpy as np
+from sklearn.model_selection import GridSearchCV
 
 def get_flight_seasonality_graph(data, time_column, grouping_column):
     """Generates line graph with count of values in *data* by *time_column* and *grouping_column*.
@@ -60,3 +61,56 @@ def get_delay_rate_graph(data, grouping_column):
     fig.set_ylabel("Proportion of Delayed Flights")
     fig.set_xlabel(grouping_column)
     plt.show()
+
+def get_to_hub_flag(flight, foreign_hubs):
+    """Checks if given flight is made by a foreign company to their hub.
+
+    :param flight: Information about the flight to be checked.
+    :type flight: Series
+    :param foreign_hubs: Foreign companies and the city on which their hub is located.
+    :type foreign_hubs: DataFrame
+    :return: True if the flight is made by a foreign company to their hub.
+    :rtype: boolean
+    """
+    try:
+        hub = foreign_hubs.loc[foreign_hubs['OPERA'] == flight['OPERA'], 'SIGLADES'].values[0]
+        return hub == flight['SIGLADES']
+    except:
+        return False
+
+def encode_feats(encoder, feature_df):
+    """Encodes features in *feature_df* using *encoder+
+
+    :param encoder: Fitted Encoder
+    :type encoder: Encoder
+    :param feature_df: Original feature DataFrame
+    :type feature_df: DataFrame
+    :return: Encoded feature DataFrame
+    :rtype: DataFrame
+    """
+    return pd.DataFrame(data=encoder.transform(feature_df), columns=encoder.get_feature_names_out())
+
+def get_best_model_CV(X_train, y_train, classifier, param_grid, scoring = 'f1_micro', n_jobs = -1):
+    """Finds the best parameter combination in *param_grid* for *classifier* through cross validation.
+
+    :param X_train: Input features for the model.
+    :type X_train: array-like
+    :param y_train: Target labels.
+    :type y_train: array-like
+    :param classifier: Model to train and optimize.
+    :type classifier: sklearn model
+    :param param_grid: Grid with possible parameter values for *classifier*
+    :type param_grid: dict
+    :param scoring: Scoring function used to evaluate the model, defaults to 'f1_micro'
+    :type scoring: str, optional
+    :param n_jobs: Number of cores to use for cross-validated grid search, defaults to -1
+    :type n_jobs: int, optional
+    :return: Best model found trained
+    :rtype: sklearn model
+    """
+
+    clf = GridSearchCV(classifier, param_grid=param_grid, scoring = scoring, n_jobs = n_jobs, verbose = True)
+    
+    best_clf = clf.fit(X_train, y_train)
+
+    return best_clf
